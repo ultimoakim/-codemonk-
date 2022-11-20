1147ab41487fcc903636e804f648b5ad41233f80
kristina-lim <kristinahlim@gmail.com>
2022-11-19 19:36:57 -0800

    Do CSS and  functionality

--- codemonk/main_app/urls.py
+++ codemonk/main_app/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
+  path('about/', views.about, name='about'),
+  path('challenges/', views.challenges_index, name='index'),
+  path('challenges/<int:challenge_id>/', views.challenges_detail, name='detail'),
+  path('challenges/create/', views.ChallengeCreate.as_view(), name='challenges_create'),
+  path('challenges/<int:pk>/update/', views.ChallengeUpdate.as_view(), name='challenges_update'),
+  path('challenges/<int:pk>/delete/', views.ChallengeDelete.as_view(), name='challenges_delete'),
 ]
--- codemonk/main_app/views.py
+++ codemonk/main_app/views.py
@@ -1,7 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Challenge
 
 # Create your views here.
 # Define the home view
 def home(request):
   # Include an .html file extension - unlike when rendering EJS templates
   return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def challenges_index(request):
+    challenges = Challenge.objects.all()
+    return render(request, 'challenges/index.html', {
+        'challenges': challenges
+    })
+
+def challenges_detail(request, challenge_id):
+    challenge = Challenge.objects.get(id=challenge_id)
+    return render(request, 'challenges/detail.html', {
+        'challenge': challenge
+    })
+
+class ChallengeCreate(CreateView):
+  model = Challenge
+  fields = '__all__'
+  success_url = '/challenges'
+
+class ChallengeUpdate(UpdateView):
+  model = Challenge
+  fields = '__all__'
+
+class ChallengeDelete(DeleteView):
+  model = Challenge
+  success_url = '/challenges'

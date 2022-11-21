ec4129d966370002cf76d33e59a88837504cc819
ultimoakim <akandykim234@gmail.com>
2022-11-21 11:28:47 -0800

    add full-CRUD functionality for challenges

--- codemonk/main_app/models.py
+++ codemonk/main_app/models.py
@@ -1,7 +1,17 @@
 from django.db import models
+from django.urls import reverse
+# Import the user
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Challenge(models.Model):
     title = models.CharField(max_length=100)
     date = models.DateTimeField(auto_now_add=True)
     description = models.TextField(max_length=250)
+    # Add ManyToManyField here (later)
+    
+    # Add the foreign key linking to a user instance
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'challenge_id': self.id})

b5632df0edc9c5e59a485d08c537d7615f3acbaf
ultimoakim <akandykim234@gmail.com>
2022-11-21 15:00:14 -0800

    add auth

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Challenge
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 # Define the home view
@@ -17,22 +21,50 @@ def challenges_index(request):
         'challenges': challenges
     })
 
+@login_required
+def challenges_user_index(request):
+    challenges = Challenge.objects.filter(user=request.user)
+    return render(request, 'challenges/userindex.html', {
+        'challenges': challenges
+    })
+
 def challenges_detail(request, challenge_id):
     challenge = Challenge.objects.get(id=challenge_id)
     return render(request, 'challenges/detail.html', {
         'challenge': challenge
     })
 
-class ChallengeCreate(CreateView):
+
+class ChallengeCreate(LoginRequiredMixin, CreateView):
   model = Challenge
-  fields = '__all__'
+  fields = ['title', 'description']
   # success_url = '/challenges'
 
-class ChallengeUpdate(UpdateView):
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+
+
+class ChallengeUpdate(LoginRequiredMixin, UpdateView):
   model = Challenge
   fields = '__all__'
+    
 
-
-class ChallengeDelete(DeleteView):
+class ChallengeDelete(LoginRequiredMixin, DeleteView):
   model = Challenge
   success_url = '/challenges'
+
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)

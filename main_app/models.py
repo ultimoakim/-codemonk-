30825f85e34c6180035158fecde152481bffc87e
CoreySumma <CoreySumma@gmail.com>
2022-11-21 18:28:31 -0600

    Add Comment Model

--- main_app/models.py
+++ main_app/models.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 # Import the user
 from django.contrib.auth.models import User
 
-# Create your models here.
+
 class Challenge(models.Model):
     title = models.CharField(max_length=100)
     date = models.DateTimeField(auto_now_add=True)
@@ -15,3 +15,15 @@ class Challenge(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'challenge_id': self.id})
+
+
+class Comment(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    description = models.CharField(max_length=400)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    challenge = models.ForeignKey(
+        Challenge,
+        on_delete=models.CASCADE
+    )
+    
+    
